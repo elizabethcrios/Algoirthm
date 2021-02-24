55420ffdbc15fffe0e3732c005f8255be75d1825
Elizabeth Rios <59344894+elizabethcrios@users.noreply.github.com>
2021-02-23 21:41:46 -0700

    Update matrixChainOrder.java

--- matrixChainOrder.java
+++ matrixChainOrder.java
@@ -1,36 +1,44 @@
+import java.util.Arrays;
+
 //code taken from https://www.programmersought.com/article/48616580/ 
 public class matrixChainOrder {
-	
+
 	
 		private static String name = "ABCDEF";
-		private static int[] a = {40, 20, 30, 10, 30};
-		
-		private static int len = a.length - 1;
-		
-		private static int[][] m = new int[len][len];
-		private static int[][] s = new int[len][len];
-		
+	
 		public static void main(String[] args)
 		{
+			int value = 0; 
+			int[] game = new int[args.length];
+			for (int i = 0; i < args.length; i++)
+	            game[i] = Integer.parseInt(args[i]);
+			int len = game.length - 1;
+			int[][] s = new int[len][len];
+			int[][] m = new int[len][len];
+			
+			int optimal_value = Compute(game, m, s, value);
+			
 			System.out.println("Hope this works: ");
 			System.out.println("Value tables is:");
 			for(int i=0; i<len; i++) {
 				for(int j = 0; j < len; j++) {
 					System.out.print(m[i][j] + " ");
 				}
+				System.out.println("");
 			}
-			
+			System.out.println("");
 			System.out.println("Decision tables is:");
 			for(int i=1; i<len; ++i) {
 				for(int j = 1; j < len; ++j) {
 					System.out.print(s[i][j] + " ");
 				}
+				System.out.println(" ");
 			}
 			
-			int value = 0; 
-			int optimal_value = Compute(a, m, s, value);
+			
+			
 			System.out.print ("minimum number of calculations required:");
-			Compute(a, m, s, value );
+			Compute(game, m, s, value );
 			System.out.println(optimal_value);
 			
 			 System.out.print("The order in which the matrices are multiplied is: ");
@@ -85,5 +93,6 @@ public class matrixChainOrder {
 			}
 		}
 	}
+
 
 
